2423d70f878b869193c09d3cf720f7d2d7b8b755
Bob Waltenspiel <forestplay@gmail.com>
2020-07-13 23:01:20 -0700

    changed to accect only one parameter

--- error-handling/error_handling.sh
+++ error-handling/error_handling.sh
@@ -3,18 +3,14 @@
 usage="Usage: error_handling.sh <person>"
 
 main () {
-if [[ $# -eq 0 ]];then
-  echo $usage
-  exit 1
-elif [[ $# -gt 1 && $2 != "and" ]];then
-  echo $usage
+if (( $# != 1 ));then
+  echo "$usage"
   exit 1
 fi
 
-echo "Hello, "$*
+echo "Hello, $1"
 
 }
 
 # call main with all of the positional arguments
 main "$@"
-
