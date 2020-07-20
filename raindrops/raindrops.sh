ab217da06712835180260670692472c0814d59bd
Bob Waltenspiel <forestplay@gmail.com>
2020-07-20 15:50:16 -0700

    Changed raindrops.sh after feedback from exercism mentor, simplified result

--- raindrops/raindrops.sh
+++ raindrops/raindrops.sh
@@ -1,24 +1,8 @@
 #!/usr/bin/env bash
 
-main () {
-    outString=""
-    for value in $@; do
-        if (( $value % 3 == 0 ));then
-            outString=${outString}"Pling"
-        fi
-        if (( $value % 5 == 0 ));then
-            outString=${outString}"Plang"
-        fi
-        if (( $value % 7 == 0 ));then
-            outString=${outString}"Plong"
-        fi
-
-        if [[ $outString ]];then
-            echo $outString
-        else
-            echo $value
-        fi
-    done
-}
-
-main "$@"
+for value in $@; do
+  (("$value" % 3 == 0)) && outString+="Pling"
+  (("$value" % 5 == 0)) && outString+="Plang"
+  (("$value" % 7 == 0)) && outString+="Plong"
+  echo ${outString:-"$value"}
+done
