9e08ed0645c5b0d6c45a88fbf446abcf986fd9b8
Bob Waltenspiel <forestplay@gmail.com>
2020-07-21 13:19:16 -0700

    Simplified for loop in raindrops.sh

--- raindrops/raindrops.sh
+++ raindrops/raindrops.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-for value in $@; do
+for value do
   (("$value" % 3 == 0)) && outString+="Pling"
   (("$value" % 5 == 0)) && outString+="Plang"
   (("$value" % 7 == 0)) && outString+="Plong"
