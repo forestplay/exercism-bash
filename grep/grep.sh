f2d655e90e8b1eb7f2e5055d8772b1322010b73d
Bob Waltenspiel <forestplay@gmail.com>
2020-07-22 11:22:35 -0700

    Changed how quotes are used to eliminate warnings from IDE.

--- grep/grep.sh
+++ grep/grep.sh
@@ -1,46 +1,49 @@
 #!/usr/bin/env bash
 
 # Set defaults - empty string == false
-printLineNumbers=""     # if true, print the line numbers of each matching line.
-printNamesOnly=""       # if true, print only the names of files that contain at least one matching line.
-caseSensitive=""        # if true, match line using a case-insensitive comparison.
-invertPrint=""          # if true, invert the program -- collect all lines that fail to match the pattern.
-matchLine=""            # if true, only match entire lines, instead of lines that contain a match.
-multipleFiles=""        # if true, output file name before matching line.
-debug=""                # if true, detailed messages throughout script
+printLineNumbers="" # if true, print the line numbers of each matching line.
+printNamesOnly=""   # if true, print only the names of files that contain at least one matching line.
+caseSensitive=""    # if true, match line using a case-insensitive comparison.
+invertPrint=""      # if true, invert the program -- collect all lines that fail to match the pattern.
+matchLine=""        # if true, only match entire lines, instead of lines that contain a match.
+multipleFiles=""    # if true, output file name before matching line.
+debug=""            # if true, detailed messages throughout script
 
 while getopts ":dnlivx" opt; do
   case $opt in
-    n) printLineNumbers="true" ;;
-    l) printNamesOnly="true" ;;
-    i) caseSensitive="true"; shopt -s nocasematch ;;
-    v) invertPrint="true" ;;
-    x) matchLine="true" ;;
-    d) debug="true" ;;
-    \?)
-      echo $(basename "$0")": Invalid option: -$OPTARG" >&2
-      exit 1
-      ;;
+  n) printLineNumbers="true" ;;
+  l) printNamesOnly="true" ;;
+  i)
+    caseSensitive="true"
+    shopt -s nocasematch
+    ;;
+  v) invertPrint="true" ;;
+  x) matchLine="true" ;;
+  d) debug="true" ;;
+  \?)
+    echo "$(basename "$0"): Invalid option: -$OPTARG" >&2
+    exit 1
+    ;;
   esac
 done
-shift $((OPTIND-1))
+shift $((OPTIND - 1))
 
 match() {
   [[ ($invertPrint && ! $1 =~ $2) || (! $invertPrint && $1 =~ $2) ]]
 }
 
-main () {
+main() {
   local regex=$1
   shift
   [[ $matchLine ]] && regex="^$regex$"
-  (( $# > 1 )) && multipleFiles="true"
+  (($# > 1)) && multipleFiles="true"
 
-  for file in "$@";do
-    (( lineNumber=0 ))
+  for file in "$@"; do
+    ((lineNumber = 0))
     while IFS= read -r line; do
-      (( lineNumber=lineNumber+1 ))
+      ((lineNumber = lineNumber + 1))
       if match "$line" "$regex"; then
-        if [[ $printNamesOnly ]];then
+        if [[ $printNamesOnly ]]; then
           printf "%s\n" "$file"
           break
         fi
@@ -48,18 +51,18 @@ main () {
         [[ $printLineNumbers ]] && printf '%s' "$lineNumber:"
         printf '%s\n' "$line"
       fi
-    done < "$file"
+    done <"$file"
   done
 }
 
 if [[ $debug ]]; then
-  echo "##### Starting "$(basename "$0")
-  echo "PrintLineNumbers:" $([[ "$printLineNumbers" ]] && echo "$printLineNumbers" || echo "false")
-  echo "  PrintNamesOnly:" $([[ "$printNamesOnly" ]] && echo "$printNamesOnly" || echo "false")
-  echo "   CaseSensitive:" $([[ "$caseSensitive" ]] && echo "$caseSensitive" || echo "false")
-  echo "     InvertPrint:" $([[ "$invertPrint" ]] && echo "$invertPrint" || echo "false")
-  echo "       MatchLine:" $([[ "$matchLine" ]] && echo "$matchLine" || echo "false")
-  echo "        parameters:" "$@"
+  echo "##### Starting $(basename "$0")"
+  echo "PrintLineNumbers: $([[ $printLineNumbers ]] && echo $printLineNumbers || echo false)"
+  echo "  PrintNamesOnly: $([[ $printNamesOnly ]] && echo $printNamesOnly || echo false)"
+  echo "   CaseSensitive: $([[ $caseSensitive ]] && echo $caseSensitive || echo false)"
+  echo "     InvertPrint: $([[ $invertPrint ]] && echo $invertPrint || echo false)"
+  echo "       MatchLine: $([[ $matchLine ]] && echo $matchLine || echo false)"
+  echo "      parameters: $*"
   echo
 fi
 
